3d510b70919c45dc4cb85ffe059d0a5a208967ae
ganznz <loganbellinghamlb@gmail.com>
2025-06-05 21:45:34 +1200

    completed tut 6 demo 3

--- 06-conditional-deployments/06-tut3.ps1
+++ 06-conditional-deployments/06-tut3.ps1
@@ -1,8 +1,8 @@
 $rgName = "arm-intro-06";
 
 New-AzResourceGroupDeployment `
-	-Name "arm-intro-06.2-deployment" `
+	-Name "arm-intro-06.3-deployment" `
 	-ResourceGroupName $rgName `
-	-TemplateFile "06-conditional-deployments/06-tut2.json" `
+	-TemplateFile "06-conditional-deployments/06-tut3.json" `
 	-Force `
 	-Verbose
